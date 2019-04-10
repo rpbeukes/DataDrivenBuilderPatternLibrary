1c9f108e7d293077a1bbb9bb810c5b273db22e9b
Ruan Beukes <ruan.beukes@readify.net>
2019-04-10 20:32:23 +0800

    add some samples

---
+++ Data.Models/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BuilderLibraryTests.DataModels
+{
+    public class Address
+    {
+        public int StreetNumber { get; set; }
+        public string Suburb { get; set; }
+        public string PostCode { get; set; }
+    }
+}
---
+++ Data.Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderLibraryTests.DataModels
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<Address> Addresses { get; set; }
+    }
+}
