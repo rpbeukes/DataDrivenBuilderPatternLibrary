d84e96f36a4672bac98e665bc63ea84506e02934
Ruan Beukes <ruan.beukes@readify.net>
2019-04-10 18:40:33 +0800

    add some code

---
+++ BuilderLibrary/BuilderBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BuilderLibrary
+{
+    public class BuilderBase<TBuildResult, TBuilder> : IBuilder<TBuildResult, TBuilder>
+        where TBuildResult : class, new()
+        where TBuilder : class, IBuilder
+    {
+        protected TBuildResult _concreteObject = new TBuildResult();
+
+        public TBuildResult Build()
+        {
+            return _concreteObject;
+        }
+
+        public TBuilder With(Action<TBuildResult> setAction)
+        {
+            setAction?.Invoke(_concreteObject);
+            return this as TBuilder;
+        }
+    }
+}
---
+++ BuilderLibrary/IBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BuilderLibrary
+{
+    public interface IBuilder {  /* maker to indicate a builder object */ }
+
+    public interface IBuilder<TBuildResult, TBuilder> : IBuilder
+        where TBuildResult : class, new()
+        where TBuilder : class, IBuilder
+    {
+        /// <summary>
+        /// A generic way to set properties
+        /// </summary>
+        TBuilder With(Action<TBuildResult> setAction); TBuildResult Build();
+    }
+}
